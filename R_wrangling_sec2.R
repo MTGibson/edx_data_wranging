c2b9fa86189768f67d70294a92d0efe71b038321
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2021-01-10 13:45:19 -0500

    adding section 2.2 as sec 2 was getting too long. Wrapped up sec2.

--- R_wrangling_sec2.R
+++ R_wrangling_sec2.R
@@ -20,4 +20,123 @@ filename <- file.path(path,  "fertility-two-countries-example.csv")
 wide_data <- read_csv(filename)
 select(wide_data, country, `1960`:`1967`)
 
-# blug
+# Reshaping Data
+
+# original wide data
+library(tidyverse) 
+path <- system.file("extdata", package="dslabs")
+filename <- file.path(path,  "fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+
+# tidy data from dslabs
+library(dslabs)
+data("gapminder")
+tidy_data <- gapminder %>% 
+  filter(country %in% c("South Korea", "Germany")) %>%
+  select(country, year, fertility)
+
+# gather wide data to make new tidy data
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, `1960`:`2015`)
+head(new_tidy_data)
+
+# gather all columns except country
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country)
+
+# gather treats column names as characters by default
+class(tidy_data$year)
+class(new_tidy_data$year)
+
+# convert gathered column names to numeric
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country, convert = TRUE)
+class(new_tidy_data$year)
+
+# ggplot works on new tidy data
+new_tidy_data %>%
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
+
+# spread tidy data to generate wide data
+new_wide_data <- new_tidy_data %>% spread(year, fertility)
+select(new_wide_data, country, `1960`:`1967`)
+
+
+# import data
+path <- system.file("extdata", package = "dslabs")
+filename <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+raw_dat <- read_csv(filename)
+select(raw_dat, 1:5)
+
+# gather all columns except country
+dat <- raw_dat %>% gather(key, value, -country)
+head(dat)
+dat$key[1:5]
+
+# separate on underscores
+dat %>% separate(key, c("year", "variable_name"), "_")
+dat %>% separate(key, c("year", "variable_name"))
+
+# split on all underscores, pad empty cells with NA
+dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"), 
+                 fill = "right")
+
+# split on first underscore but keep life_expectancy merged
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
+# separate then spread
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") %>%
+  spread(variable_name, value) 
+
+# separate then unite
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_")
+
+# full code for tidying data
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
+  spread(variable_name, value) %>%
+  rename(fertility = fertility_NA)
+
+rm(list=ls())
+
+
+data("CO2")
+
+
+
+library(dslabs)
+data(admissions)
+dat <- admissions %>% select(-applicants)
+
+
+## Testing out spread
+
+#Which command could help you to wrangle the data into the desired format?
+#  dat_tidy1 <- spread(dat, major, admitted)
+#  dat_tidy2 <- spread(dat, gender, major)
+  dat_tidy <- spread(dat, gender, admitted)
+#  dat_tidy4 <- spread(dat, admitted, gender)
+  
+  
+tmp <- gather(admissions, key, value, admitted:applicants)
+tmp
+
+#Combine the key and gender and create a new column called column_name to get a variable 
+#with the following values: admitted_men, admitted_women, applicants_men and applicants_women. Save the new data as tmp2.
+#Which command could help you to wrangle the data into the desired format?
+  
+#tmp2_a <- spread(tmp, column_name, key, gender)
+#tmp2_b <- gather(tmp, column_name, c(gender,key))
+#tmp2_c <- unite(tmp, column_name, c(gender, key))
+#tmp2_d <- spread(tmp, column_name, c(key,gender))
+tmp2 <- unite(tmp, column_name, c(key, gender))
+
+#Which function can reshape tmp2 to a table with six rows and five columns named major, admitted_men, admitted_women, applicants_men and applicants_women?
+
+tmp3<-spread(tmp2, column_name, value)
+
+
