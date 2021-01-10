814ecd0fc7f6cc6b44fb5da226464ff068a1552e
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2021-01-10 14:33:00 -0500

    Added sec 2.3 as a new R script because it focses on webscrapping, which I know I need help with

--- R_wrangling_sec2_2.R
+++ R_wrangling_sec2_2.R
@@ -37,3 +37,60 @@ tab1 %>% right_join(tab2)
 inner_join(tab1, tab2)
 semi_join(tab1, tab2)
 anti_join(tab1, tab2)
+
+### Questions 5-7 
+
+library(Lahman)
+top <- Batting %>% 
+  filter(yearID == 2016) %>%
+  arrange(desc(HR)) %>%    # arrange by descending HR count
+  slice(1:10)    # take entries 1-10
+top %>% as_tibble()
+
+#Also Inspect the Master data frame, which has demographic information for all players:
+  
+  Master %>% as_tibble()
+  
+  
+  
+#  Question 5
+#  1 point possible (graded)
+#  Use the correct join or bind function to create a combined table of the names and 
+#  statistics of the top 10 home run (HR) hitters for 2016. This table should have the player ID,
+# first name, last name, and number of HR for the top 10 players. Name this data frame top_names.
+  
+# Identify the join or bind that fills the blank in this code to create the correct table:
+    
+  top_names <- top %>% left_join(Master) %>%
+    select(playerID, nameFirst, nameLast, HR)
+  
+  top_names
+  
+  
+ # Question 6
+#  1 point possible (graded)
+#  Inspect the Salaries data frame. Filter this data frame to the 2016 salaries, then use 
+#  the correct bind join function to add a salary column to the top_names data frame from the
+#  previous question. Name the new data frame top_salary. Use this code framework:
+    
+    
+    top_salary <- Salaries %>% filter(yearID == 2016) %>%
+    right_join(top_names) %>%
+    select(nameFirst, nameLast, teamID, HR, salary)
+    
+    top_salary
+    
+    AwardsPlayers
+    arrange(desc(HR)) %>%    # arrange by descending HR count
+      slice(1:10)    # take entries 1-10
+    
+    bat<-Batting %>% filter(yearID==2016) %>% select(playerID, yearID, HR)%>%arrange(desc(HR))
+    bat
+    
+    ap<-AwardsPlayers %>% filter(yearID== 2016) %>% select(playerID, awardID)
+    
+    combined<-ap %>% left_join(bat)%>% arrange(desc(HR))
+    combined<-combined %>% left_join(top_names)
+    
+combined<-combined%>%group_by(playerID)%>%summarize(n=n(), topten=!is.na(nameFirst))
+combined
