8044e555dcb632d654521e5aacc49b25bdf64bec
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2021-01-21 14:59:08 -0500

    Finished the string processing unit

--- R_wrangling_sec3.R
+++ R_wrangling_sec3.R
@@ -308,5 +308,24 @@ index <- str_detect(remaining_problems, pattern)
 remaining_problems[!index]
 
 
+library(rvest)
+library(tidyverse)
+library(stringr)
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+tab <- read_html(url) %>% html_nodes("table")
+polls <- tab[[5]] %>% html_table(fill = TRUE)
 
+names(polls) <- c("dates", "remain", "leave", "undecided", "lead", "samplesize", "pollster", "poll_type", "notes")
+polls <- polls[str_detect(polls$remain, "%"), -9]
+nrow(polls)
+polls$remain<-as.numeric(str_replace(polls$remain, "%", ""))/100
+polls$leave<-as.numeric(str_replace(polls$leave, "%", ""))/100
+
+#polls$remain<-parse_number(polls$remain)/100
+
+polls$undecided<-str_replace(polls$undecided,"N/A","0")
+polls$undecided<-as.numeric(str_replace(polls$undecided, "%", ""))/100
+
+temp <- str_extract_all(polls$dates, "\\d+\\s[a-zA-Z]+")
+end_date <- sapply(temp, function(x) x[length(x)])
 
